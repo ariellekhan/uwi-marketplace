5afe83f55d8add92987ef7184b44ba55512007ea
ariellekhan <40500873+ariellekhan@users.noreply.github.com>
2019-01-30 14:47:04 -0400

    Validation for uwi email added

--- lib/app_screens/signup.dart
+++ lib/app_screens/signup.dart
@@ -3,14 +3,24 @@ import 'login.dart';
 
 class SignUp extends StatefulWidget {
   static String tag = 'signup-page';
+
   @override
   _SignUpState createState() => new _SignUpState();
 }
 
 class _SignUpState extends State<SignUp> {
-
   // dropdown menu lists
-  List _degreeLevels = ["B.A.", "B.Sc.", "M.A.", "M.Sc.", "MBA", "M.Phil.", "Ph.D.", "M.D", "DDS"];
+  List _degreeLevels = [
+    "B.A.",
+    "B.Sc.",
+    "M.A.",
+    "M.Sc.",
+    "MBA",
+    "M.Phil.",
+    "Ph.D.",
+    "M.D",
+    "DDS"
+  ];
   List _faculties = [
     "Science and Technology",
     "Food and Agriculture",
@@ -21,9 +31,17 @@ class _SignUpState extends State<SignUp> {
     "Law",
   ];
 
+//  _formKey and _autoValidate
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _autoValidate = false;
+  String _email;
+  String _name;
+  String _phone;
+  String _major;
+  String _password;
+
   @override
   Widget build(BuildContext context) {
-
     final email = TextFormField(
       keyboardType: TextInputType.emailAddress,
       autofocus: false,
@@ -32,11 +50,10 @@ class _SignUpState extends State<SignUp> {
         contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
       ),
-//      validator: (value) {
-//        if (value.isEmpty) {
-//          return 'Please enter some text';
-//        }
-//      },
+      validator: validateEmail,
+      onSaved: (String val) {
+        _email = val;
+      },
     );
 
     final name = TextFormField(
@@ -46,6 +63,9 @@ class _SignUpState extends State<SignUp> {
         contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
       ),
+      onSaved: (String val) {
+        _name = val;
+      },
     );
 
     final phone = TextFormField(
@@ -56,6 +76,9 @@ class _SignUpState extends State<SignUp> {
         contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
       ),
+      onSaved: (String val) {
+        _phone = val;
+      },
     );
 
     final major = TextFormField(
@@ -65,6 +88,9 @@ class _SignUpState extends State<SignUp> {
         contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
       ),
+      onSaved: (String val) {
+        _major = val;
+      },
     );
 
     final password = TextFormField(
@@ -75,8 +101,10 @@ class _SignUpState extends State<SignUp> {
         contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
       ),
+      onSaved: (String val) {
+        _password = val;
+      },
     );
-
 
     final signupButton = Padding(
       padding: EdgeInsets.symmetric(vertical: 16.0),
@@ -84,19 +112,21 @@ class _SignUpState extends State<SignUp> {
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(24),
         ),
-        onPressed: () {
-          Navigator.of(context).pushNamed(LoginPage.tag);
-        },
+//        onPressed: () {
+//          Navigator.of(context).pushNamed(LoginPage.tag);
+//        },
+        onPressed: _validateInputs,
         padding: EdgeInsets.all(12),
         color: Colors.lightBlueAccent,
         child: Text('Sign Up!', style: TextStyle(color: Colors.white)),
       ),
     );
 
-    final heading = Padding (
+    final heading = Padding(
       padding: EdgeInsets.all(10.0),
-      child: Text("Create an Account",
-         style: TextStyle(color: Colors.lightBlueAccent, fontSize: 24),
+      child: Text(
+        "Create an Account",
+        style: TextStyle(color: Colors.lightBlueAccent, fontSize: 24),
       ),
     );
 
@@ -107,48 +137,75 @@ class _SignUpState extends State<SignUp> {
         ),
         onPressed: () {
           Navigator.of(context).pushNamed(LoginPage.tag);
-        }
-    );
+        });
 
-
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(title: Text('UWI MARKETPLACE'), backgroundColor: Colors.lightBlueAccent,),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 40),
-        child: ListView(
-          shrinkWrap: true,
-          padding: EdgeInsets.only(left: 24.0, right: 24.0),
-          children: <Widget>[
-            heading,
-            SizedBox(height: 24.0),
-            Row(
-              children: <Widget>[
-                Expanded (
-                  child: name,
-                ),
-                Expanded (
-                  child: Padding(
-                    padding: const EdgeInsets.only(left:8),
-                    child: phone,
+    return Form(
+      key: _formKey,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('UWI MARKETPLACE'),
+          backgroundColor: Colors.lightBlueAccent,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 40),
+          child: ListView(
+            shrinkWrap: true,
+            padding: EdgeInsets.only(left: 24.0, right: 24.0),
+            children: <Widget>[
+              heading,
+              SizedBox(height: 24.0),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: name,
                   ),
-                ),
-              ],
-
-            ),
-            SizedBox(height: 8.0),
-            email,
-            SizedBox(height: 8.0),
-            major,
-            SizedBox(height: 8.0),
-            password,
-            SizedBox(height: 24.0),
-            signupButton,
-            accountLabel,
-          ],
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8),
+                      child: phone,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 8.0),
+              email,
+              SizedBox(height: 8.0),
+              major,
+              SizedBox(height: 8.0),
+              password,
+              SizedBox(height: 24.0),
+              signupButton,
+              accountLabel,
+            ],
+          ),
         ),
       ),
     );
-
   }
+
+  String validateEmail(String value) {
+//    Pattern pattern =
+//        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+//    RegExp regex = new RegExp(pattern);
+//    if (!regex.hasMatch(value))
+//      return 'Enter Valid Email';
+    if(!value.endsWith("@my.uwi.edu") && !value.endsWith("@sta.uwi.edu"))
+      return 'Enter Valid UWI Email';
+    else
+      return null;
+  } // validateEmail
+
+  void _validateInputs() {
+    if (_formKey.currentState.validate()) {
+//    If all data are correct then save data to out variables
+      _formKey.currentState.save();
+    } else {
+//    If all data are not valid then start auto validation.
+      setState(() {
+        _autoValidate = true;
+      });
+    }
+  } // _validateInputs
+
 }
