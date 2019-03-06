1f0342119c98f11a55fa770bf9f80b4183be00e0
rahulraychaudhuri <43577982+rahulraychaudhuri@users.noreply.github.com>
2019-03-06 14:46:18 -0400

    Revert home to login page

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       navigatorObservers: <NavigatorObserver>[observer],
-      home: Profile(),
+      home: LoginPage(),
       routes: routes,
     );
   }
