fc1370a9bb89f19e1f14949a03345dd7f6467fac
sachinpanchoo <sachinpanchoo22@gmail.com>
2019-03-10 09:10:48 -0700

    all items added

--- lib/app_screens/item_details.dart
+++ lib/app_screens/item_details.dart
@@ -1,1 +1,96 @@
 import 'package:flutter/material.dart';
+import '../item_information.dart';
+
+
+class ItemDetails extends StatefulWidget{
+  final ItemInfo itemInfo;
+  ItemDetails({Key key, @required this.itemInfo}) : super(key: key);
+
+  @override
+  _ItemDetailsState createState() => new _ItemDetailsState();
+
+}
+
+class _ItemDetailsState extends State<ItemDetails> {
+  @override
+  Widget build(BuildContext context) {
+    String _imageUrl = widget.itemInfo.imageUrl;
+    final img = Container(
+        padding: EdgeInsets.only(right: 12.0),
+
+        child:_imageUrl == "" ? Image.asset('images/placeholder.png', height: 200.0,width: 200.0,)
+            :Image.network(_imageUrl,height: 200.0, width: 200.0,),
+      );
+
+    final name = Center(
+      child: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Text(
+          '${widget.itemInfo.name}',
+          style: TextStyle(color: Colors.deepOrangeAccent, fontSize: 24),
+        ),
+      ),
+    );
+
+    final cost = Center(
+      child: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Text(
+            r'$''${widget.itemInfo.price}',
+          style: TextStyle(color: Colors.green, fontSize: 24),
+        ),
+      ),
+    );
+
+    final description = Center(
+      child: Padding(
+        padding: EdgeInsets.all(5.0),
+        child: Text(
+          'Description: ${widget.itemInfo.description}',
+          style: TextStyle(color: Colors.black, fontSize: 19),
+        ),
+      ),
+    );
+
+    final orderButton = Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(50),
+        ),
+        onPressed: () {
+
+
+        },
+        padding: EdgeInsets.all(12),
+        color: Colors.orangeAccent,
+        child: Text('Buy', style: TextStyle(color: Colors.white)),
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Item Details"),
+        backgroundColor: Colors.orangeAccent,
+      ),
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 24.0, right: 24.0),
+          children: <Widget>[
+            img,
+            SizedBox(height: 8.0),
+            name,
+            SizedBox(height: 24.0),
+            cost,
+            SizedBox(height: 8.0),
+            description,
+            SizedBox(height: 8.0),
+
+            orderButton,
+          ],
+        ),
+      ),
+    );
+  }
+}
